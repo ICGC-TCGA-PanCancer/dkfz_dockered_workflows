6841974e2672632c26680a9a2c34c5d8742cc5b5
lindaxiang <linda.xiang@oicr.on.ca>
2019-03-29 22:19:45 -0400

    add sanity check for run-id

--- Dockstore.cwl
+++ Dockstore.cwl
@@ -14,7 +14,7 @@ dct:contributor:
 
 requirements:
 - class: DockerRequirement
-  dockerPull: quay.io/pancancer/pcawg-dkfz-workflow:2.1.0
+  dockerPull: quay.io/pancancer/pcawg-dkfz-workflow:standard-output-names
 
 cwlVersion: v1.0
 
--- scripts/run_workflow.pl
+++ scripts/run_workflow.pl
@@ -34,8 +34,14 @@ GetOptions (
 if ($run_id eq "")
 {
   $run_id = get_aliquot_id_from_bam($tumor_bam);
-  print "run-id is: $run_id\n";
 }
+
+if ($run_id =~ /^[a-zA-Z0-9_-]+$/) {
+    print "run-id is: $run_id\n";
+} else {
+    die "Found run-id contains invalid character: $run_id\n";
+}
+
 
 # PARSE OPTIONS
 system("sudo chmod a+rwx /tmp");
